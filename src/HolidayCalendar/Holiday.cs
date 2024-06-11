8ae2d37dc47ef4e69983ff60d1d42d0fff6a6012
Thomas Lind <thli49474@edu.ucl.dk>
2024-06-11 19:33:02 +0200

    My solution added

---
+++ src/HolidayCalendar/Holiday.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolidayCalendar
+{
+	public class Holiday
+	{
+		public DateTime date { get; set; }
+		public string name { get; set; }
+		public bool nationalHoliday { get; set; }
+	}
+}
--- src/HolidayCalendar/HolidayCalendar.cs
+++ src/HolidayCalendar/HolidayCalendar.cs
@@ -1,18 +1,42 @@
 ﻿using System;
 using System.Collections;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace HolidayCalendar;
 public class HolidayCalendar : IHolidayCalendar
 {
+	private static HttpClient _httpClient = new()
+	{
+		BaseAddress = new Uri("https://api.sallinggroup.com"),
+		DefaultRequestHeaders = { Authorization = new AuthenticationHeaderValue("Bearer", "e99e6a68-e717-4245-b1e4-790011fc09c7") }
+	};
+
   public bool IsHoliday(DateTime date)
   {
-    // TODO - replace the below exception with your own implementation
-    throw new NotImplementedException();
+	  //Send request
+	  var task = _httpClient.GetFromJsonAsync<bool>($"/v1/holidays/is-holiday?date={date:yyyy-MM-dd}");
+	  task.Wait();
+
+	  return task.Result;
   }
 
   public ICollection<DateTime> GetHolidays(DateTime startDate, DateTime endDate)
   {
-    // TODO - replace the below exception with your own implementation
-    throw new NotImplementedException();
+	  //Send request
+	  var task = _httpClient.GetFromJsonAsync<List<Holiday>>(
+		  $"/v1/holidays?startDate={startDate:yyyy-MM-dd}&endDate={endDate:yyyy-MM-dd}");
+	  task.Wait();
+		
+	  //Make result list
+	  var result = new List<DateTime>();
+	  //Add dates for holidays to result
+	  foreach (var holiday in task.Result.Where(h => h.nationalHoliday == true))
+	  {
+		  result.Add(holiday.date);
+	  }
+
+	  return result;
   }
 }
